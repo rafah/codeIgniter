b6f7d21b5cd6fd379c4ed80a40bc20b9ae37b48d
Rafael Acioly <rafael.acioly@outlook.com>
2013-10-22 13:23:57 -0200

    First Commit

---
+++ application/controllers/olaMundo.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class OlaMundo extends CI_Controller {
+
+	public function index(){
+		echo __FUNCTION__;
+	}
+	public function incluir(){
+		echo __FUNCTION__;
+	}
+}
